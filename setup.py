5719bda6707c2ccc5fbaf773976f51c885cb71d8
odadoun <odadoun@gmail.com>
2021-07-12 18:03:32 +0200

    add the setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+""" 
+Project : DadXenonStuff
+Date :    July2021
+Authors : Olivier Dadoun
+dadoun@in2p3.fr
+"""
+
+from setuptools import setup, find_packages
+setup(
+    # Needed for dependencies
+    install_requires=[ \
+        'bokeh',\
+        'pandas',\
+        ],
+)
